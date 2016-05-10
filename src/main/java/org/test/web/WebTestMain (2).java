968991b5dfbbe074365b13576bd118219ef54c42
zhaoshb <zhaoshengbo@126.com>
2016-05-10 12:16:33 +0800

    packaging jar!

---
+++ src/main/java/org/test/web/WebTestMain (2).java
@@ -0,0 +1,13 @@
+package org.test.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTestMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebTestMain.class, args);
+	}
+
+}
